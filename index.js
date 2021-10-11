67b34f12b48df509d12ddd94c9ff5d42c9a5460d
Kennedy <kennedybaird@gmail.com>
2021-10-11 15:14:06 +1300

    nvm

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
 const TOKEN = process.env.TOKEN;
+
+const binary = require('decode-encode-binary')
 
 bot.login(TOKEN);
 
+
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
-  });
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => msg.author.bot ? true : msg.reply(binary.auto(msg.content,true)));
